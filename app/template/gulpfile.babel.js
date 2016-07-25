089dc190727d62f713119a95d99de3aef84003b8
Felix Kalkuhl <felix@kalkuhl.net>
2016-07-25 22:28:40 +0200

    image optim task

--- app/template/gulpfile.babel.js
+++ app/template/gulpfile.babel.js
@@ -6,20 +6,58 @@ import argv     from 'yargs';
 
 var flag = argv.argv;
 
+// possible flags
+//
+//  / operation options
+//    --dev    | development mode
+//    --test   | test mode 
+//    --prod   | test production mode
+//
+//  / deploy and server
+//    --server | set virtual server
+//    --deploy | auto deploys
+//    --watch  | watches
+
 const paths = {
   styles: {
     src: 'src/**/*.scss',
     dest: 'dist/css/'
   }
+  iamges: {
+    src: 'src/img/**/*',
+    // srcOriginal: 'src/img/orginals', ?
+    dest: 'dist/img/'
+  }
 };
 
-export function styles() {
+function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass())
-    .pipe(gulpIf(flag.boo, notify("ok")))
+    .pipe(gulpIf(flag.boo, notify('ok')))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
+function images() {
+  return gulp.src(paths.images.src, {since: gulp.lastRun('images')})
+    .pipe(imagemin({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true, 
+      svgoPlugins: [{cleanupIDs: false}]
+    }))
+    .pipe(gulp.dest(paths.images.src));
+    .pipe(gulp.dest(paths.images.dest));
+    .pipe(notify('images compressed and copied'))
+}
+images.description = 'Compressing Images in src and copy them into dist';
+
+
 const test = styles;
 
-export default test;
+// tasks
+//
+// default (auto --prod)
+// watch (auto --dev)
+
+
+export default styles;
