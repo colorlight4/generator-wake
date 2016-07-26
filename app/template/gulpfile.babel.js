0807de304f01f8f655223448619cb4dc1dbf4aca
Felix Kalkuhl <felix@kalkuhl.net>
2016-07-26 22:54:50 +0200

    minfiy task

--- app/template/gulpfile.babel.js
+++ app/template/gulpfile.babel.js
@@ -1,14 +1,18 @@
 import browserSync  from 'browser-sync';
 import gulp         from 'gulp';
-import sass         from 'gulp-sass';
 import notify       from 'gulp-notify';
 import gulpIf       from 'gulp-if';
-import imagemin     from 'gulp-imagemin';
+import plumber      from 'gulp-plumber';
 import include      from 'gulp-include';
 import sourcemaps   from 'gulp-sourcemaps';
-import plumber      from 'gulp-plumber';
 import argv         from 'yargs';
 import del          from 'del';
+import sass         from 'gulp-sass';
+import imagemin     from 'gulp-imagemin';
+import htmlmin      from 'gulp-htmlmin';
+import cleanCSS     from 'gulp-clean-css';
+import uglify       from 'gulp-uglify';
+import rename       from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 
 var flag = argv.argv;
@@ -52,46 +56,61 @@ const paths = {
 const clean = () => del([ 'dist' ]);
 export { clean };
 
-export function inject() { // cool aber subotimal weil immer beide geparst werden
-  return gulp.src([ paths.html.src, paths.js.src ])
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+export function html() {
+  return gulp.src(paths.html.src)
+    .pipe( plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe( include({
       hardFail: true
     }))
-    .pipe( gulp.dest('dist/'))
-    .pipe( notify('inject passed'))
+    .pipe( gulp.dest(paths.html.dest))
+    .pipe( notify('html passed'))
     .pipe( reload({stream:true}));
 }
 
-export function watchInject() {
-  gulp.watch([ paths.html.src, paths.js.src ], inject)
-};
-
 export function styles() {
   return gulp.src(paths.styles.src)
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe( plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe( sass())
     .pipe( gulp.dest(paths.styles.dest))
     .pipe( notify('styles passed'))
     .pipe( reload({stream:true}));
 }
 
+// export function js() {
+//   return gulp.src(paths.js.src)
+//     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+//     .pipe( include({
+//       hardFail: true
+//     }))
+//     .pipe( gulp.dest(paths.js.dest))
+//     .pipe( notify('js passed'))
+//     .pipe( reload({stream:true}));
+// }
+
 export function images() {
   return gulp.src(paths.images.src, {since: gulp.lastRun('images')})
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-    .pipe(imagemin({
+    .pipe( plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe( imagemin({
       optimizationLevel: 5,
       progressive: true,
       interlaced: true, 
       svgoPlugins: [{cleanupIDs: false}]
     }))
-    .pipe(gulp.dest(paths.images.src))
-    .pipe(gulp.dest(paths.images.dest))
-    .pipe(notify('images compressed and passed'))
+    .pipe( gulp.dest(paths.images.src))
+    .pipe( gulp.dest(paths.images.dest))
+    .pipe( notify('images compressed and passed'))
     .pipe( reload({stream:true}));
 }
 images.description = 'Compressing Images in src and copy them into dist';
 
+export function minify() {
+  return gulp.src('dist/**/*')
+    .pipe( doIf('*.js', uglify()))
+    .pipe( doIf('*.css', cleanCSS()))
+    .pipe( doIf('*.html', htmlmin({collapseWhitespace: true})))
+    .pipe( rename({suffix: '.min'}))
+    .pipe( gulp.dest('dist/'));
+}
 
 const server = () => browserSync({ server: { baseDir: 'dist/' } });
 
